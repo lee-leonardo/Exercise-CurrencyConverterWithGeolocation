f57ae3018e36c0ac21fc9e788450f58fbf4c6f5b
lee-leonardo <lee.leonardo.king@gmail.com>
2021-07-22 22:52:18 -0700

    error handling code, check the two conversion types, wrap in a try catch to catch errors on our end.

--- server/app.js
+++ server/app.js
@@ -17,24 +17,48 @@ app.get('/api/currencies', (req, res) => {
  * Returns the converted currency amount.
 */
 app.get('/api/currencyConverter', (req, res) => {
-  const fromCurrency = req.query.from;
-  const toCurrency = req.query.to;
-  const amountCurrency = req.query.amount;
+  try {
+    const fromCurrency = req.query.from;
+    const toCurrency = req.query.to;
+    const amountCurrency = req.query.amount;
 
-  const rates = Object.values(ratesByCountryName);
+    const rates = Object.values(ratesByCountryName);
 
-  const fromConversionRate = rates.find(rate => rate.currencyCode === fromCurrency);
-  const toConversionRate = rates.find(rate => rate.currencyCode === toCurrency);
-  const newConversionRate = 1.0 / fromConversionRate['rateFromUSDToCurrency'] * toConversionRate['rateFromUSDToCurrency'];
+    const fromConversionRate = rates.find(rate => rate.currencyCode === fromCurrency);
+    const toConversionRate = rates.find(rate => rate.currencyCode === toCurrency);
 
-  const amountInToCurrency = amountCurrency * newConversionRate;
+    // if undefined need to
+    if (!fromConversionRate) {
+      // log message about missing currency
 
-  const responseObject = {
-    amount: amountInToCurrency,
-    conversionRate: newConversionRate,
+      res.status(400).send({
+        message: 'unable to find conversion rate from currency: ' + fromCurrency
+      });
+    } else if (!toConversionRate) {
+      // log message about missing currency
+
+      res.status(400).send({
+        message: 'unable to find conversion rate from currency: ' + toCurrency
+      });
+    }
+
+    const newConversionRate = 1.0 / fromConversionRate['rateFromUSDToCurrency'] * toConversionRate['rateFromUSDToCurrency'];
+
+    const amountInToCurrency = amountCurrency * newConversionRate;
+
+    const responseObject = {
+      amount: amountInToCurrency,
+      conversionRate: newConversionRate,
+    }
+
+    return res.status(200).send(responseObject);
+  } catch (e) {
+    // log e.message
+
+    return res.status(500).send({
+      message: 'something unexpected happened.'
+    })
   }
-
-  return res.status(200).send(responseObject);
 });
 
 /*
