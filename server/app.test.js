d830fa7e2c653febd707517eda184a2cc38320ca
lee-leonardo <lee.leonardo.king@gmail.com>
2021-07-22 23:55:02 -0700

    base test spec, for expediency

--- server/app.test.js
+++ server/app.test.js
@@ -19,7 +19,31 @@ describe('Test /api/currencyConverter', () => {
     expect(response.statusCode).toBe(200);
     expect(response.body.amount).toBe(975.6);
   });
+
+  test('Show error when user supplies fromCurrency');
+
+  test('Show error when user supplies toCurrency');
+
+  test('Show error when user supplies unsupported toCurrency');
+
+  test('Show error when user supplies unsupported toCurrency');
 });
 
 describe('Test /api/locationToCurrency', () => {
+
+  test('Returns correct country code for US');
+
+  test('Returns correct country code for Mexico');
+
+  test('Returns correct country code for super accurate');
+
+  test('Returns correct country code for vague address');
+
+  test('Show 400 error when lat is not given');
+
+  test('Show 400 error when long is not given');
+
+  test('Show 500 error when downstream api fails');
+
+
 });
